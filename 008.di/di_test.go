942cc5c57d8d7c8aea4631271c3eb0aa0de15bcd
Jigar Thacker <jigar.thacker@veritas.com>
2021-04-13 20:01:04 +0530

    add dependency injection

---
+++ 008.di/di_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// func TestGreet(t *testing.T) {
+//     buffer := bytes.Buffer{}
+//     Greet(&buffer, "Chris")
+
+//     got := buffer.String()
+//     want := "Hello, Chris"
+
+//     if got != want {
+//         t.Errorf("got %q want %q", got, want)
+//     }
+// }
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Jigar")
+
+	got := buffer.String()
+	want := "Hello, Jigar"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
